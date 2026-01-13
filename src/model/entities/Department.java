6e0a6bdf429ea53b6d33450e1eecec597398d41b
Giuliano Neto <Giuliano_neto@outlook.com>
2026-01-13 18:13:33 -0300

    Seller entity class

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,4 +1,53 @@
 package model.entities;
 
-public class Department {
+import java.io.Serializable;
+
+public class Department implements Serializable {
+    private Integer id;
+    private String name;
+
+    public Department() {}
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Department that = (Department) o;
+        return id.equals(that.id) && name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" + "id=" + id + ", name=" + name + '}';
+    }
+
+
 }
