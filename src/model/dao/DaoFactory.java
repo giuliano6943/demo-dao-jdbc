d9cbbdb39cc4c9657d45a0c0d3852171d1c589bf
Giuliano Neto <Giuliano_neto@outlook.com>
2026-01-14 11:51:23 -0300

    Reusing instantiation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -7,7 +7,18 @@ import model.entities.Seller;
 //Cria e retorna um obj SellerDaoJDBC
 public class DaoFactory {
 
+    /*
+SellerDaoJDBC → é a classe que sabe conversar com o banco de dados (via JDBC).
+
+DB → é a classe que sabe abrir uma conexão com o banco MySQL.
+
+DaoFactory → é a “fábrica” que cria objetos DAO prontos para uso.
+
+    */
+
     public static SellerDao createSellerDao() {
+        //DB.getConnection() Abre ou reutiliza uma conexão com o banco de dados
+        //new SellerDaoJDBC cria um DAO passando essa conexão
         return new SellerDaoJDBC(DB.getConnection());
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -14,12 +14,12 @@ import java.util.List;
 //Classe que vai implementar de verdade os métodos da interface SellerDao
 public class SellerDaoJDBC implements SellerDao {
 
+    //Criando a conexão com o Banco de dados
     private Connection conn;
-
+    //Construtor que tras essa conexão vinda da classe DB
     public SellerDaoJDBC(Connection conn) {
         this.conn = conn;
     }
-
 
     @Override
     public void insert(Seller obj) {
@@ -42,24 +42,22 @@ public class SellerDaoJDBC implements SellerDao {
         PreparedStatement st = null;
         ResultSet rs = null;
         try {
+            //Montando uma querySQL que busca o vendedor pelo ID
+            //O ? é substituido pelo valor do parametro no setInt
             st = conn.prepareStatement(
-                    "SELECT seller.*,department.Name as DepName\n" +
+                        "SELECT seller.*,department.Name as DepName\n" +
                             "FROM seller INNER JOIN department\n" +
                             "ON seller.DepartmentId = department.Id\n" +
                             "WHERE seller.Id = ?");
             st.setInt(1,id);
             rs = st.executeQuery();
+            //Verificando se existe algum registro retornado
             if(rs.next()){
-                Department dep = new Department();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("DepName"));
-                Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setName(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setBaseSalary(rs.getDouble("BaseSalary"));
-                obj.setBirthday(rs.getDate("BirthDate"));
-                obj.setDepartment(dep);
+                //Convertendo o resultado da consulta em objetos Java, Department e Seller
+                //Está dizendo que basicamente altere o Id e o nome do objeto dep pelo Id e Name da tabela.
+                //E a mesma coisa com Seller
+                Department dep = instantiateDepartment(rs);
+                Seller obj = instantiateSeller(rs, dep);
                 return obj;
             }
             return null;
@@ -71,6 +69,25 @@ public class SellerDaoJDBC implements SellerDao {
             DB.closeStatement(st);
             DB.closeResultSet(rs);
         }
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller obj =new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setBaseSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthday(rs.getDate("BirthDate"));
+        obj.setDepartment(dep);
+        return obj;
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+        return dep;
     }
 
     @Override
